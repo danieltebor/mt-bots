08a6ea150d301ec7864965cf8fe8731c8ace7d23
Daniel Tebor <danieltebor@gmail.com>
2025-06-04 19:34:19 -0400

    Update manager intent

--- run_bot.py
+++ run_bot.py
@@ -19,14 +19,14 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 if not (parser.parse_args().manager or parser.parse_args().media_player):
-    raise ValueError("You must specify either --manager or --media_player")
+    raise ValueError('You must specify either --manager or --media_player')
 elif parser.parse_args().manager:
-    bot = Manager(intents=intents, presence_msg="I passed the Captcha")
+    bot = Manager(intents=intents, presence_msg='I broke the cutting board')
 else:
     bot = MediaPlayer(intents=intents)
 
 token_var = parser.parse_args().token_var
 token = os.getenv(token_var)
 if token is None:
-    raise ValueError(f"No token found for {token_var} in .env file.")
+    raise ValueError(f'No token found for {token_var} in .env file.')
 bot.run(token)
